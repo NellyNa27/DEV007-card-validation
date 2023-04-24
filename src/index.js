2dff72f0215f7a0e487ddadedd582439e8064f20
NellyNa27 <nlly.rro@gmail.com>
2023-04-24 13:58:05 -0600

    arreglo1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
+//importar para llamar de otro js
+import validator from "./validator.js";
+const botonV = document.getElementById("boton-V");
+const username = document.getElementById("username");
+const cardNumber = document.getElementById("cardNumber");
 
-import validator from './validator.js';
-//Crear boton
-const boton.
-
-
-//Función que captura datos de un formulario
-
-function validator(){
-const usuario=document.getElementById("nombreUsuario").value;
-const noTarjeta=document.getElementById("numeroDeTarjeta").value;
-if(usuario==" ") {
-  alert("Llenar")
-}
-
-console.log(usuario + " " + noTarjeta);
-}
-
-//Mostrar numero de tarjeta mostrando todos los numeros, excepto los ultimos 4
-// 123456789012####
-
-console.log("validator");
-
+botonV.addEventListener("click", () => {
+  validator.isValid(cardNumber.value, username.value);
+});
--- src/validator.js
+++ src/validator.js
@@ -1,14 +1,47 @@
-const validator= {
-  isValid: function(creditCardNumber){
-    console.log("voy a validar" + creditCardNumber)
-  },
+const validator = {
+  // mensajeDesdeIndex(msj){
+  //   alert("mensaje recibido:" + msj)
+  // },
 
-
-
-  maskify: function(creditCardNumber){
-    console.log("voy a enmascara"+creditCardNumber)
-
-  },
-}
-
-validator
+  isValid: function (creditCardNumber, username) {
+    alert("voy a validar: " + creditCardNumber + " de: " + username);
+    console.log(creditCardNumber);
+    //Array de los numeros de tarjeta ingresados
+    const cardNumberArray = Array.from(creditCardNumber);
+    console.log(cardNumberArray);
+    //Pasar los numeros del array en orden inverso
+    const cardNumberArrayReversed = cardNumberArray.reverse();
+    console.log(cardNumberArrayReversed);
+    //Aplicar operación a los numeros de las posiciones pares, multiplicado x2.
+    for (let i = 0; i < cardNumberArrayReversed.length; i++) {
+      //verificar si es impar -
+      if (i % 2 !== 0) {
+        cardNumberArrayReversed[i] = cardNumberArrayReversed[i]*2;
+        console.log (cardNumberArrayReversed);
+        //suma de digitos despues de multiplicar...
+        if (cardNumberArrayReversed[i] >= 10) {
+          //pasarlo a string/mas de un elemento -->
+          const cardReversed = cardNumberArrayReversed[i].toString();
+          console.log(cardReversed);
+        //Guardar numero en dos dígitos-string
+        cardNumberArrayReversed[i]=(parseInt(cardReversed[0]))+(parseInt(cardReversed)[1]);
+      // convertirlos nuevamente en número para sumar los dos digitos.
+      console.log(cardReversed);
+      }
+      }
+      /*if (cardNumberArrayReversed[i] >= 10) {
+        //pasarlo a string/mas de un elemento -->
+        const cardReversed = cardNumberArrayReversed.toString();
+        console.log(cardNumberArrayReversed);
+      //Guardar numero en dos dígitos-string
+      cardNumberArrayReversed[i]=(parseInt(cardReversed[0]))+(parseInt(cardReversed)[1]);
+    // convertirlos nuevamente en número para sumar los dos digitos.
+    console.log(cardReversed);
+    }*/
+          }}
+        }
+      //maskify: function(creditCardNumber) {
+        //console.log("voy a enmascarar:" + creditCardNumber);
+      //}
+  //}
+export default validator;
